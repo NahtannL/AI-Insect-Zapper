ec13aa0c6d4432fcc4e560c16f710ef80bdfa506
NahtannL <thenathan168@gmail.com>
2025-07-16 00:36:55 -0700

    add conversion code to TensorRT

---
+++ model-training/README.md
@@ -0,0 +1,6 @@
+## Training
+Training is done to the existing YOLOv11 model and trained with various datasets listed below
+
+* People (Humans)
+* Insects (Cockroach, Fly, Hornet, Spider)
+* Mosquitoes
--- model-training/training.py
+++ model-training/training.py
@@ -1,3 +1,9 @@
+# For training the model using YOLO11 as the base.
+#
+# The 'data.yaml' file contains the file paths to the dataset used for
+# training. The completed model is stored in the runs folder found in the root
+# of this project (AI-Insect-Zapper/runs/...)
+
 from ultralytics import YOLO
 
 model = YOLO('yolo11n.yaml')
